686deec2c0ecc9c091d5bd0447bcc18f90a2090b
Abu <63474683+Abumuazu@users.noreply.github.com>
2020-06-17 21:16:54 -0700

    Updates

---
+++ precache-manifest.fb548e0bf5e2d00ca9f4efda305c3fa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62b3105992226f227e42dc1d5f13965",
+    "url": "/%20//Abumuazu.github.io/monsters-roladex/index.html"
+  },
+  {
+    "revision": "d0537f34f2e605a5cb32",
+    "url": "/%20//Abumuazu.github.io/monsters-roladex/static/css/main.2dd2b149.chunk.css"
+  },
+  {
+    "revision": "d0e91b20846f0b628908",
+    "url": "/%20//Abumuazu.github.io/monsters-roladex/static/js/2.cccd965e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%20//Abumuazu.github.io/monsters-roladex/static/js/2.cccd965e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0537f34f2e605a5cb32",
+    "url": "/%20//Abumuazu.github.io/monsters-roladex/static/js/main.eb18d375.chunk.js"
+  },
+  {
+    "revision": "6966eab329d489868535",
+    "url": "/%20//Abumuazu.github.io/monsters-roladex/static/js/runtime-main.d2a27b2b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2bc8c5c05476684816384622407a1f5.js"
+  "/%20//Abumuazu.github.io/monsters-roladex/precache-manifest.fb548e0bf5e2d00ca9f4efda305c3fa3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%20//Abumuazu.github.io/monsters-roladex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
